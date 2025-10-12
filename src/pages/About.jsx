072bef6d91c20441aa6a45a8f78e91f1c389281d
aldo1725 <rifaldo.s1704@gmail.com>
2025-10-12 20:05:33 +0700

    fgf

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,7 @@ export default function About() {
   return (
     <section
       id="About"
-      className="py-20 md:py-28 scroll-mt-[130px] bg-white" // ubah margin scroll biar posisinya pas di atas
+      className="py-20 md:py-28 scroll-mt-[60px] bg-white" // ubah margin scroll biar posisinya pas di atas
     >
       <div className="max-w-5xl mx-auto px-6">
         {/* Judul */}
