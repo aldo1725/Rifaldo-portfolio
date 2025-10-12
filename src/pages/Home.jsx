9d9c0778125b5925f9ef0d989de67a0fa8950295
aldo1725 <rifaldo.s1704@gmail.com>
2025-10-12 20:10:43 +0700

    dfddf

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import avatar from "../assets/avatar.svg";
 
 export default function Home() {
   return (
-    <section id="home" className="bg-gray-50 scroll-mt-[80px]">
+    <section id="home" className="bg-gray-50 scroll-mt-[140px]">
       <div className="max-w-6xl mx-auto px-6 py-24 md:py-28 grid md:grid-cols-2 gap-10 items-center">
         
         {/* Bagian kiri - teks */}
