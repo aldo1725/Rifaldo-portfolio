41f2290e17bc100620cc7d45226953af1660fbeb
aldo1725 <rifaldo.s1704@gmail.com>
2025-10-13 12:49:00 +0700

    py-8

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -67,7 +67,7 @@ export default function Skills() {
   ];
 
   return (
-    <section id="Skills" className="py-10 md:py-14 bg-gray-50 scroll-mt-[80px]">
+    <section id="Skills" className="py-10 md:py-8 bg-gray-50 scroll-mt-[80px]">
       <div className="max-w-6xl mx-auto px-6">
         {/* Judul */}
         <motion.h2
