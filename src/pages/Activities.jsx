25dfdf1c602447718bbdcb34e9775eee47f7cffb
aldo1725 <rifaldo.s1704@gmail.com>
2025-10-13 19:31:28 +0700

    pp

--- src/pages/Activities.jsx
+++ src/pages/Activities.jsx
@@ -20,7 +20,7 @@ export default function Activities() {
     slidesToShow: 1,
     slidesToScroll: 1,
     centerMode: true,
-    centerPadding: "20px",
+    centerPadding: "60px",
     pauseOnHover: true,
     arrows: false,
     cssEase: "ease-in-out",
