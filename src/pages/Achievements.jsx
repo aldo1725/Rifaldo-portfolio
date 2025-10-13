0f6fe7c8cf6a34d1446fda2a5c771971945c1844
aldo1725 <rifaldo.s1704@gmail.com>
2025-10-13 13:02:58 +0700

    revisi ke tiga

--- src/pages/Achievements.jsx
+++ src/pages/Achievements.jsx
@@ -90,7 +90,7 @@ export default function Achievements() {
   return (
     <section
       id="Achievements"
-      className="py-12 sm:py-16 md:py-10 bg-white overflow-hidden scroll-mt-[60px]"
+      className="py-16 md:py-24 bg-white overflow-hidden scroll-mt-[140px]"
     >
       <div className="max-w-6xl mx-auto px-6">
         {/* Judul */}
